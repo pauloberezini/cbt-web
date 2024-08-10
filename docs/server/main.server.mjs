ae155f868f3ae0978be6cd4fda7dea9092febf10
Pavel Bereznichenko <paulo.berezini@gmail.com>
2024-08-10 23:14:44 +0300

    updated text for tarot master

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SQCXW35X.mjs";import"./chunk-4HVPPZQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7DPZTUZM.mjs";import"./chunk-4HVPPZQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
